ae33205f476e50e8253c12a344f8daa9560300dd
David Vilela <dvilelaf@gmail.com>
2023-05-12 16:37:49 +0100

    feat: multiplexer

---
+++ packages/valory/skills/multiplexer_abci/models.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2023 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This module contains the shared state for the abci skill of MultiplexerAbciApp."""
+
+from typing import Any, List
+
+from packages.valory.skills.abstract_round_abci.models import BaseParams
+from packages.valory.skills.abstract_round_abci.models import (
+    BenchmarkTool as BaseBenchmarkTool,
+)
+from packages.valory.skills.abstract_round_abci.models import Requests as BaseRequests
+from packages.valory.skills.abstract_round_abci.models import (
+    SharedState as BaseSharedState,
+)
+from packages.valory.skills.multiplexer_abci.rounds import MultiplexerAbciApp
+from aea.skills.base import SkillContext
+
+class SharedState(BaseSharedState):
+    """Keep the current shared state of the skill."""
+
+    abci_app_cls = MultiplexerAbciApp
+
+    def __init__(self, *args: Any, skill_context: SkillContext, **kwargs: Any) -> None:
+        """Init"""
+        super().__init__(*args, skill_context=skill_context, **kwargs)
+
+        self.task_queue: List = []
+
+
+class Params(BaseParams):
+    """Parameters."""
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Initialize the parameters object."""
+
+        self.post_hour_utc = self._ensure("post_hour_utc", kwargs, int)
+
+        super().__init__(*args, **kwargs)
+
+
+Requests = BaseRequests
+BenchmarkTool = BaseBenchmarkTool
