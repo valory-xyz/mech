5dff16c570138ef75727d3718affbb67a1c90acd
David Vilela <dvilelaf@gmail.com>
2023-05-13 13:38:47 +0100

    feat: run service script

---
+++ run_service.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Load env vars
+export $(grep -v '^#' .env | xargs)
+
+# Remove previous builds
+# if [ -d "governatooorr" ]; then
+#     echo $PASSWORD | sudo -S sudo rm -Rf governatooorr;
+# fi
+
+# Push packages and fetch service
+# make formatters
+# make generators
+make clean
+
+autonomy push-all
+
+autonomy fetch --local --service eightballer/mech && cd mech
+
+# Build the image
+autonomy build-image
+
+# Copy keys and build the deployment
+cp /home/david/Valory/env/keys.json ./keys.json
+
+autonomy deploy build -ltm
+
+# Run the deployment
+autonomy deploy run --build-dir abci_build/
