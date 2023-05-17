d0c2b69dcfc4e31aa986dc37104d3237cc6ecdf3
David Minarsch <david.minarsch@googlemail.com>
2023-05-17 12:37:12 +0100

    chore: fix hashes

--- packages/fetchai/protocols/default/message.py
+++ packages/fetchai/protocols/default/message.py
@@ -27,8 +27,7 @@ from aea.configurations.base import PublicId
 from aea.exceptions import AEAEnforceError, enforce
 from aea.protocols.base import Message
 
-from packages.fetchai.protocols.default.custom_types import \
-    ErrorCode as CustomErrorCode
+from packages.fetchai.protocols.default.custom_types import ErrorCode as CustomErrorCode
 
 _default_logger = logging.getLogger("aea.packages.fetchai.protocols.default.message")
 
