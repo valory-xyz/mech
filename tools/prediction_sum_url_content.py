06dde74f50fdb0694cfb43a3a4fdc965b312e2c7
Jannik Hehemann <jannik@hehemann-melle.de>
2023-09-23 11:18:52 +0200

    feat: Changed prompts to give more concise and clear instructions. GPT-4 is able to give p_no = 1 when specified event deadline exceeded and no information indicating for p_yes are found.

--- tools/prediction_sum_url_content.py
+++ tools/prediction_sum_url_content.py
@@ -48,27 +48,32 @@ ALLOWED_TOOLS = [
 ]
 TOOL_TO_ENGINE = {
     "prediction-offline-sum-url-content": "gpt-3.5-turbo",
-    "prediction-online-sum-url-content": "gpt-3.5-turbo",
-    # "prediction-online-sum-url-content": "gpt-4",
+    # "prediction-online-sum-url-content": "gpt-3.5-turbo",
+    # "prediction-online-sum-url-content": "gpt-3.5-turbo-16k",
+    "prediction-online-sum-url-content": "gpt-4",
 }
 
 PREDICTION_PROMPT = """
-You are an LLM inside a multi-agent system that takes in a prompt of a user requesting a probability estimation
-for a given event. You are provided with an input under the label "USER_PROMPT". You must follow the instructions
-under the label "INSTRUCTIONS". You must provide your response in the format specified under "OUTPUT_FORMAT".
+You are an LLM inside a multi-agent system. Your task is to estimate the probability of a user's 'event question', 
+which specifies an event in the physical world and any accompanying conditions to be met for the 'event question' to be true. The 'event question' allows only two outcomes: the event 
+will either occur or not, given the conditions. Find the 'event question' enclosed in double quotes as a part of 
+the user's prompt under 'USER_PROMPT'. The user's prompt also contains a more elaborate description of the task. 
+You are provided an itemized list of information under the label "ADDITIONAL_INFORMATION", delimited by three backticks. 
+You must adhere to the 'INSTRUCTIONS'.  
 
-INSTRUCTIONS
-* Read the input under the label "USER_PROMPT" delimited by three backticks.
-* The "USER_PROMPT" specifies an event.
-* The event will only have two possible outcomes: either the event will happen or the event will not happen.
-* If the event has more than two possible outcomes, you must ignore the rest of the instructions and output the response "Error".
-* You must provide a probability estimation of the event happening, based on your training data.
-* You are provided an itemized list of information under the label "ADDITIONAL_INFORMATION" delimited by three backticks.
-* You can use any item in "ADDITIONAL_INFORMATION" in addition to your training data.
-* Given today's date {today_date} you should use predominantly the more recent information in "ADDITIONAL_INFORMATION" to make your probability estimation.
-* You must pay very close attention to the specific wording of the question in "USER_PROMPT" 
-* If a date is provided in the USER_PROMPT for the event to have occured, you must also consider in your estimation, given today's date {today_date}, how likely it is that the event will occur before or on that provided date.
+
+INSTRUCTIONS:
+* Carefully read the user's prompt under 'USER_PROMPT', enclosed by triple backticks.
+* If the 'event question' has more than two outcomes, respond with "Error" and ignore further instructions.
+* Based on your training data, provide a probability estimation of the event specified in the 'event question' occuring, considering all conditions provided.
+* You can use any item in "ADDITIONAL_INFORMATION" in addition to your training data to make the probability estimation.
+* Prioritize recent information in "ADDITIONAL_INFORMATION" based on the current time {timestamp}.
+* You must pay very close attention to the specific wording of the 'event question' in "USER_PROMPT".
+* If a date is provided in the 'event question' specifying when the event has to have occured, you must consider in your estimation, given the current time {timestamp}, how likely it is that the event will occur within the remaining timespan to that provided date.
 * If an item in "ADDITIONAL_INFORMATION" is not relevant for the estimation, you must ignore that item.
+* If there is insufficient information in "ADDITIONAL_INFORMATION", be aware of the limitations of your training data especially when relying on it for predicting events that require up-to-date information. So make a prediction that takes into account that you don't have up-to-date information.
+* Your pobability estimation must not only take into account if the specified event happens or not, but also if the event is likely to happen before or on the date specified in the 'event question'.
+* If the 'event question' is formulated in a way that an event must have happend by or before a specific date, consider the deadline of the event being 23:59:59 of that date. Decrease the probability of the event specified in the 'event question' happening the closer the current time {timestamp} is to the deadline, if you could not find information that the event could happen within the remaining time. If the remaining time is 0, decrease the probability to 0.
 * You must provide your response in the format specified under "OUTPUT_FORMAT".
 * Do not include any other contents in your response.
 
@@ -82,45 +87,41 @@ ADDITIONAL_INFORMATION:
 {additional_information}
 ```
 
-OUTPUT_FORMAT
+OUTPUT_FORMAT:
 * Your output response must be only a single JSON object to be parsed by Python's "json.loads()".
-* The JSON must contain four fields: "p_yes", "p_no", "confidence", and "info_utility".
-* Each item in the JSON must have a value between 0 and 1.
-   - "p_yes": Estimated probability that the event in the "USER_PROMPT" occurs.
-   - "p_no": Estimated probability that the event in the "USER_PROMPT" does not occur.
-   - "confidence": A value between 0 and 1 indicating the confidence in the prediction. 0 indicates lowest
-     confidence value; 1 maximum confidence value.
-   - "info_utility": Utility of the information provided in "ADDITIONAL_INFORMATION" to help you make the prediction.
-     0 indicates lowest utility; 1 maximum utility.
+* The JSON must contain four fields: "p_yes", "p_no", "confidence", and "info_utility", each ranging from 0 to 1.
+   - "p_yes": Estimated probability that the event specified in the 'event question' occurs, considering all conditions provided.
+   - "p_no": Estimated probability that the 'event question' does not occur, considering all conditions provided.
+   - "confidence": Indicating the confidence in the estimated probabilities you provided ranging from 0 (lowest confidence) to 1 (maximum confidence).
+   - "info_utility": Utility of the information provided in "ADDITIONAL_INFORMATION" to help you make the prediction ranging from 0 (lowest utility) to 1 (maximum utility).
 * The sum of "p_yes" and "p_no" must equal 1.
-* Output only the JSON object first and a short explanation (max. 3 sentences) what specificly were the most relevant information that led to your estimation. Do not include any other contents in your response.
+* Output only the JSON object in your response.
 """
 
 URL_QUERY_PROMPT = """
-You are an LLM inside a multi-agent system that takes in a prompt of a user requesting a probability estimation
-for a given event. You are provided with an input under the label "USER_PROMPT". You must follow the instructions
-under the label "INSTRUCTIONS". You must provide your response in the format specified under "OUTPUT_FORMAT".
+You are a Large Language Model in a multi-agent system. Your task is to formulate search engine queries based on 
+a user's 'event question', which specifies an event and any accompanying conditions. The 'event question' allows 
+only two outcomes: the event will either occur or not, given the conditions. Find the 'event question' under 'USER_PROMPT' 
+and adhere to the 'INSTRUCTIONS'.
 
-INSTRUCTIONS
-* Read the input under the label "USER_PROMPT", delimited by three backticks, carefully.
-* The "USER_PROMPT" specifies an event.
-* The event will only have two possible outcomes: either the event will happen or the event will not happen.
-* If the event has more than two possible outcomes, you must ignore the rest of the instructions and output the response "Error".
+INSTRUCTIONS:
+* Carefully read the 'event question' under 'USER_PROMPT', enclosed by triple backticks.
+* If the 'event question' has more than two outcomes, respond with "Error" and ignore further instructions.
+* Create a list of 1-5 unique search queries likely to yield relevant and contemporary information for assessing the event's likelihood under the given conditions.
+* Each query must be unique, and they should not overlap or yield the same set of results.
 * You must provide your response in the format specified under "OUTPUT_FORMAT".
 * Do not include any other contents in your response.
 
 USER_PROMPT:
 ```
-{user_prompt}
+{event_question}
 ```
 
-OUTPUT_FORMAT
+OUTPUT_FORMAT:
 * Your output response must be only a single JSON object to be parsed by Python's "json.loads()".
 * The JSON must contain two fields: "queries", and "urls".
-   - "queries": An array of strings of size between 1 and 5. Each string must be a search engine query that has a high chance to yield search engine results that
-     help obtain contemporary and relevant information for you to estimate the probability that the event specified in "USER_PROMPT" occurs. You must provide original information in each query, 
-     and the queries should not overlap or lead to obtain the same set of results. 
-* Output only the JSON object. Do not include any other contents in your response. 
+   - "queries": A 1-5 item array of the generated search engine queries.
+* Include only the JSON object in your output.
 """
 
 def search_google(query: str, api_key: str, engine: str, num: int = 3) -> List[str]:
@@ -155,15 +156,15 @@ def get_urls_from_queries(queries: List[str], api_key: str, engine: str) -> List
     return unique_results
 
 
-def get_website_summary(text: str, prompt: str, model, tokenizer, nlp, max_words: int = 150) -> str:
+def get_website_summary(text: str, event_question: str, model, tokenizer, nlp, max_words: int = 150) -> str:
     """Get text summary from a website"""    
     # Check for empty inputs
-    if not prompt or not text:
+    if not event_question or not text:
         return ""
 
     # Calculate the BERT embedding for the prompt
     with torch.no_grad():
-        question_tokens = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True)
+        question_tokens = tokenizer(event_question, return_tensors="pt", padding=True, truncation=True)
         question_embedding = model(**question_tokens).last_hidden_state.mean(dim=1)
         
     # Sentence splitting and NER
@@ -227,7 +228,7 @@ def get_date(soup):
         'pub-date', 'pub_date', 'datePublished', 'date_published',
         'time_published', 'article:published_date', 'parsely-pub-date',
         'publish-date', 'pubdatetime', 'published_time', 'publishedtime',
-        'article_date', 'created_date', 'published_at',
+        'article_date', 'created_date', 'published_at', 'lastPublishedDate'
         'og:published_time', 'og:release_date', 'article:published_time',
         'og:publication_date', 'og:pubdate', 'article:publication_date',
         'product:availability_starts', 'product:release_date', 'event:start_date',
@@ -287,7 +288,7 @@ def get_date(soup):
 
 def extract_text(
     html: str,
-    prompt: str,
+    event_question: str,
     model,
     tokenizer,
     nlp,
@@ -327,7 +328,7 @@ def extract_text(
 
     text_summary = get_website_summary(
         text=text,
-        prompt=prompt,
+        event_question=event_question,
         model=model,
         tokenizer=tokenizer,
         nlp=nlp,
@@ -348,7 +349,7 @@ def process_in_batches(
 
 def extract_texts(
     urls: List[str],
-    prompt: str,
+    event_question: str,
 ) -> List[str]:
     """Extract texts from URLs"""
     max_allowed = 45
@@ -371,7 +372,7 @@ def extract_texts(
                     continue
                 extracted_text = extract_text(
                     html=result.text,
-                    prompt=prompt,
+                    event_question=event_question,
                     model=model,
                     tokenizer=tokenizer,
                     nlp=nlp,
@@ -392,7 +393,7 @@ def extract_texts(
 
 
 def fetch_additional_information(
-    prompt: str,
+    event_question: str,
     engine: str,
     temperature: float,
     max_tokens: int,
@@ -400,7 +401,7 @@ def fetch_additional_information(
     google_engine: str,
 ) -> str:
     """Fetch additional information."""
-    url_query_prompt = URL_QUERY_PROMPT.format(user_prompt=prompt)
+    url_query_prompt = URL_QUERY_PROMPT.format(event_question=event_question)
     moderation_result = openai.Moderation.create(url_query_prompt)
     if moderation_result["results"][0]["flagged"]:
         return ""
@@ -429,7 +430,7 @@ def fetch_additional_information(
     print(f"urls: {urls}")
     texts = extract_texts(
         urls=urls,
-        prompt=prompt,
+        event_question=event_question,
     )
     additional_informations = "\n\n".join(["- " + text for text in texts])
     # print(f"additional_informations: {additional_informations}")
@@ -466,7 +467,7 @@ def run(**kwargs) -> Tuple[str, Optional[Dict[str, Any]]]:
 
     additional_information = (
         fetch_additional_information(
-            prompt=prompt,
+            event_question=event_question,
             engine=engine,
             temperature=temperature,
             max_tokens=max_tokens,
@@ -478,9 +479,9 @@ def run(**kwargs) -> Tuple[str, Optional[Dict[str, Any]]]:
     )
 
     # Get today's date and generate the prediction prompt
-    today_date = datetime.today().strftime('%Y-%m-%d')
+    timestamp = datetime.now().strftime('%Y-%m-%d')
     prediction_prompt = PREDICTION_PROMPT.format(
-        user_prompt=prompt, additional_information=additional_information, today_date=today_date,
+        user_prompt=prompt, additional_information=additional_information, timestamp=timestamp,
     )
     print(f"prediction_prompt: {prediction_prompt}\n")
 
