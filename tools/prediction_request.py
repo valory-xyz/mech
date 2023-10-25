dfd2fb16d113fa1335287fea2f24e2eabe1be073
Adamantios <adamantios.zaras@valory.xyz>
2023-10-25 16:25:38 +0300

    feat: summarize text if the corresponding tool is selected

--- tools/prediction_request.py
+++ tools/prediction_request.py
@@ -22,12 +22,23 @@
 import json
 from collections import defaultdict
 from concurrent.futures import Future, ThreadPoolExecutor
+from heapq import nlargest
+from string import punctuation
 from typing import Any, Dict, Generator, List, Optional, Tuple
 
 import openai
 import requests
+import spacy
 from bs4 import BeautifulSoup
 from googleapiclient.discovery import build
+from spacy import Language
+from spacy.cli import download
+from spacy.lang.en import STOP_WORDS
+from spacy.tokens import Doc, Span
+
+
+FrequenciesType = Dict[str, float]
+ScoresType = Dict[Span, float]
 
 
 DEFAULT_OPENAI_SETTINGS = {
@@ -43,6 +54,10 @@ TOOL_TO_ENGINE = {tool: "gpt-3.5-turbo" for tool in ALLOWED_TOOLS}
 # the default number of URLs to fetch online information for
 DEFAULT_NUM_URLS = defaultdict(lambda: 3)
 DEFAULT_NUM_URLS["prediction-online-summarized-info"] = 7
+# how much of the initial content will be kept during summarization
+DEFAULT_COMPRESSION_FACTOR = 0.05
+# the vocabulary to use for the summarization
+VOCAB = "en_core_web_sm"
 
 PREDICTION_PROMPT = """
 You are an LLM inside a multi-agent system that takes in a prompt of a user requesting a probability estimation
@@ -233,6 +248,58 @@ def fetch_additional_information(
     return "\n".join(["- " + text for text in texts])
 
 
+def load_model() -> Language:
+    """Utilize spaCy to load the model and download it if it is not already available."""
+    try:
+        return spacy.load(VOCAB)
+    except OSError:
+        print("Downloading language model...")
+        download(VOCAB)
+        return spacy.load(VOCAB)
+
+
+def calc_word_frequencies(doc: Doc) -> FrequenciesType:
+    """Get the frequency of each work in the given text, excluding stop words and punctuations."""
+    word_frequencies = defaultdict(lambda: 0)
+    for token in doc:
+        word = token.text
+        lower = word.lower()
+        if lower not in STOP_WORDS.union(punctuation):
+            word_frequencies[lower] += 1
+
+    max_frequency = max(word_frequencies.values())
+    normalized_frequencies = defaultdict(lambda: 0, {word: frequency / max_frequency for word, frequency in word_frequencies.items()})
+    return normalized_frequencies
+
+
+def calc_sentence_scores(sentence_tokens: List[Span], word_frequencies: FrequenciesType) -> ScoresType:
+    """Calculate the sentence scores."""
+    sentence_scores = defaultdict(lambda: 0)
+    for sentence in sentence_tokens:
+        for token in sentence:
+            lower = token.text.lower()
+            sentence_scores[sentence] += word_frequencies[lower]
+
+    return sentence_scores
+
+
+def summarize(text: str, compression_factor: float) -> str:
+    """Summarize the given text, retaining the given compression factor."""
+    if not text:
+        raise ValueError("Cannot summarize empty text!")
+
+    nlp = load_model()
+    doc = nlp(text)
+    word_frequencies = calc_word_frequencies(doc)
+    sentence_tokens = list(doc.sents)
+    sentence_scores = calc_sentence_scores(sentence_tokens, word_frequencies)
+    n = int(len(sentence_tokens) * compression_factor)
+    summary = nlargest(n, sentence_scores, key=sentence_scores.get)
+    summary_words = [word.text for word in summary]
+    summary_text = "".join(summary_words)
+    return summary_text
+
+
 def run(**kwargs) -> Tuple[str, Optional[Dict[str, Any]]]:
     """Run the task"""
     tool = kwargs["tool"]
@@ -240,6 +307,7 @@ def run(**kwargs) -> Tuple[str, Optional[Dict[str, Any]]]:
     max_tokens = kwargs.get("max_tokens", DEFAULT_OPENAI_SETTINGS["max_tokens"])
     temperature = kwargs.get("temperature", DEFAULT_OPENAI_SETTINGS["temperature"])
     num_urls = kwargs.get("num_urls", DEFAULT_NUM_URLS[tool])
+    compression_factor = kwargs.get("compression_factor", DEFAULT_COMPRESSION_FACTOR)
 
     openai.api_key = kwargs["api_keys"]["openai"]
     if tool not in ALLOWED_TOOLS:
@@ -256,9 +324,13 @@ def run(**kwargs) -> Tuple[str, Optional[Dict[str, Any]]]:
             kwargs["api_keys"]["google_engine_id"],
             num_urls,
         )
-        if tool == "prediction-online"
+        if tool.startswith("prediction-online")
         else ""
     )
+
+    if additional_information and tool == "prediction-online-summarized-info":
+        additional_information = summarize(additional_information, compression_factor)
+
     prediction_prompt = PREDICTION_PROMPT.format(
         user_prompt=prompt, additional_information=additional_information
     )
