bbe7cc2c4258d78fd1e4990ed608afa7706091ae
Jannik Hehemann <jannik@hehemann-melle.de>
2023-10-26 00:01:51 +0200

    refactor: Return to narrow prediction prompt template

--- tools/prediction_sentence_embedding.py
+++ tools/prediction_sentence_embedding.py
@@ -69,72 +69,29 @@ PREDICTION_PROMPT = """
 INTRODUCTION:
 You are a Large Language Model (LLM) within a multi-agent system. Your primary task is to accurately estimate the probabilities for the outcome of a 'market question', \
 found in 'USER_PROMPT'. The market question is part of a prediction market, where users can place bets on the outcomes of market questions and earn rewards if the selected outcome occurrs. The 'market question' \
-has only two possible outcomes: `Yes` or `No`. Each market has a closing date at which the outcome is evaluated. This date is typically stated within the market question.  \
-The closing date is considered to be 23:59:59 of the date provided in the market question. All market questions ask for the status of an event or a process at a specific point in time. \
-The following fictional 'EXAMPLES' demonstrate the evaluation of the outcome of a market question, given its phrasing.
-
-EXAMPLES:
-* Example 1: 
-    - Market question: "Will Donald Trump continue to be the president of the United States on 12 March 2022?"
-    - Implied status: Donald Trump is currently the president of the United States.
-    - Question asks if the status will NOT change by 12 March 2022.
-    - If status changes by 12 March 2022, the market question's outcome is `No`.
-    - If status does not change by 12 March 2022, the market question's outcome is `Yes`.
-    - If status has changed recently, and will not change again by 12 March 2022 the market question's outcome is `No`.
-    - If status will change after 12 March 2022, the market question's outcome is `Yes`.
-
-* Example 2: 
-    - Market question: "Will Colorado break its record for the hottest day by 19 November 2019?"
-    - Implied status: Colorado has not broken its record for the hottest day recently.
-    - Question asks if the status will change by 19 November 2019.
-    - If status changes by 19 November 2019, the market question's outcome is `Yes`.
-    - If status does not change by 19 November 2019, the market question's outcome is `No`.
-    - If status has changed recently, the market question's outcome is `Yes`.
-    - If status will change after 19 November 2019, the market question's outcome is `No`.
-
-* Example 3: 
-    - Market question: "Will Microsoft's new AI assistant become available to all by 11 July 2015?"
-    - Implied status: Microsoft's new AI assistant is currently not available to all.
-    - Question asks if the status will change by 11 July 2015.
-    - If status changes by 11 July 2015, the market question's outcome is `Yes`.
-    - If status does not change by 11 July 2015, the market question's outcome is `No`.
-    - If status has changed recently, the market question's outcome is `Yes`.
-    - If status will change after 11 July 2015, the market question's outcome is `No`.
-
-* Example 4:
-    - Market question: "Will Taylor Swift release a new album on 12 January 2022?"
-    - Implied status: Taylor Swift has not released a new album recently.
-    - Question asks if the status will change exactly on 12 January 2022.
-    - If status changes exactly on 12 January 2022, the market question's outcome is `Yes`.
-    - If status does not change exactly on 12 January 2022, the market question's outcome is `No`.
-    - If status has changed recently, and will not change again on 12 January 2022, the market question's outcome is `No`.
-    - If status will change after 12 January 2022, the market question's outcome is `No`.
-
-* Example 5:
-    - Market question: "Will Amazon launch a second internet satellite on or before 24 October 2023?"
-    - Implied status: Amazon has launched exactly one internet satellite.
-    - Question asks if the status will change on or before 24 October 2023.
-    - If status changes on or before 24 October 2023, the market question's outcome is `Yes`.
-    - If status does not change on or before 24 October 2023, the market question's outcome is `No`.
-    - If status has changed recently, the market question's outcome is `Yes`.
-    - If status will change after 24 October 2023, the market question's outcome is `No`.
-
-You are provided an itemized list of information under the label "ADDITIONAL_INFORMATION", which is \
+in this scenario has only two possible outcomes: `Yes` or `No`. Each market has a closing date at which the outcome is evaluated. This date is typically stated within the market question.  \
+The closing date is considered to be 23:59:59 of the date provided in the market question. If the event specified in the market question has not occurred before the closing date, the market question's outcome is `No`. \
+If the event has happened before the closing date, the market question's outcome is `Yes`. You are provided an itemized list of information under the label "ADDITIONAL_INFORMATION", which is \
 sourced from a Google search engine query performed a few seconds ago and is meant to assist you in your probability estimation. You must adhere to the following 'INSTRUCTIONS'.  
 
 
 INSTRUCTIONS:
 * Examine the user's input labeled 'USER_PROMPT'. Focus on the part enclosed in double quotes, which contains the 'market question'.
 * If the 'market question' implies more than two outcomes, output the response "Error" and halt further processing.
-* Evaluate the 'market question' and determine the implied status as well as what the question asks for.
+* When the current time {timestamp} has passed the closing date of the market and the event specified in the market question has not happened, the market question's outcome is `No` and the user who placed a bet on `No` will receive a reward.
+* When the current time {timestamp} has passed the closing date of the market and the event has happened before, the market question's final outcome is `Yes` and the user who placed a bet on `yes` will receive a reward.
+* Consider the prediction market with the market question, the closing date and the outcomes in an isolated context that has no influence on the protagonists that are involved in the event in the real world, specified in the market question. The closing date is always arbitrarily set by the market creator and has no influence on the real world. So it is likely that the protagonists of the event in the real world are not even aware of the prediction market and do not care about the market's closing date.
+* The probability estimations of the market question outcomes must be as accurate as possible, as an inaccurate estimation will lead to financial loss for the user.
 * Utilize your training data and the information provided under "ADDITIONAL_INFORMATION" to generate probability estimations for the outcomes of the 'market question'.
-* The probability estimations must be as accurate as possible, as an inaccurate estimations will lead to financial loss for the user.
-* Examine the itemized list under "ADDITIONAL_INFORMATION" thoroughly. This data is sourced from a Google search engine query done a few seconds ago. 
-* Use any item in "ADDITIONAL_INFORMATION" that is relevant for the probability estimation in addition to your training data. You can assume that you have been provided with the most current information available on the internet. Still pay close attention on the release and modification timestamps provided in parentheses right before each information item. Some information might be outdated and not relevant anymore.
-* Use the examples provided under "EXAMPLES" as a reference to unserstand how to interpret the 'market question' and how to use the information provided under "ADDITIONAL_INFORMATION".
-* More recent information within "ADDITIONAL_INFORMATION" indicated by the timestamps provided in parentheses right before each information item overrides older information within "ADDITIONAL_INFORMATION" and holds more weight for your probability estimation.
-* Even if not all information might not be released today, you can assume that there haven't been publicly available updates in the meantime except for those inside "ADDITIONAL_INFORMATION".
-* Use the curret time, which is `{timestamp}`, for your probability estimation and incorporate the remaining time until the closing date of the 'market question' into your probability estimation.
+* Examine the itemized list under "ADDITIONAL_INFORMATION" thoroughly and use all the relevant information for your probability estimation. This data is sourced from a Google search engine query done a few seconds ago. 
+* Use any relevant item in "ADDITIONAL_INFORMATION" in addition to your training data to make the probability estimation. You can assume that you have been provided with the most current and relevant information available on the internet. Still pay close attention on the release and modification timestamps provided in parentheses right before each information item. Some information might be outdated and not relevant anymore.
+* More recent information indicated by the timestamps provided in parentheses right before each information item overrides older information within ADDITIONAL_INFORMATION and holds more weight for your probability estimation.
+* If there exist contradicting information, evaluate the release and modification dates of those information and prioritize the information that is more recent and adjust your confidence in the probability estimation accordingly.
+* Even if not all information might not be released today, you can assume that there haven't been publicly available updates in the meantime except for those inside ADDITIONAL_INFORMATION.
+* If the information in "ADDITIONAL_INFORMATION" indicate without a doubt that the event has already happened, it is very likely that the outcome of the market question will be `Yes`.
+* If the information in "ADDITIONAL_INFORMATION" indicate that the event will happen after the closing date, it is very likely that the outcome of the market question will be `No`.
+* The closer the current time `{timestamp}` is to the closing time the higher the likelyhood that the outcome of the market question will be `No`, if recent information do not clearly indicate that the event will occur before the closing date.
+* If there exist recent information indicating that the event will happen after the closing date, it is very likely that the outcome of the market question will be `No`.
 * You must provide your response in the format specified under "OUTPUT_FORMAT".
 * Do not include any other contents in your response.
 
