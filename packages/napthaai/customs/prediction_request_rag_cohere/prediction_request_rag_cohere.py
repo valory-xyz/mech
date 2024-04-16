fb9c175506ab9bb7596554d7aa92de8772774f8f
cyberosa <arosacastillo@protonmail.com>
2024-04-16 17:50:53 +0200

    adding some PR comments and reducing the pytest logs to info instead of debug

--- packages/napthaai/customs/prediction_request_rag_cohere/prediction_request_rag_cohere.py
+++ packages/napthaai/customs/prediction_request_rag_cohere/prediction_request_rag_cohere.py
@@ -194,25 +194,20 @@ LLM_SETTINGS = {
     },
     "claude-3-haiku-20240307": {
         "default_max_tokens": 1000,
-        "limit_max_tokens": 200_0000,
+        "limit_max_tokens": 200_000,
         "temperature": 0,
     },
     "claude-3-sonnet-20240229": {
         "default_max_tokens": 1000,
-        "limit_max_tokens": 200_0000,
+        "limit_max_tokens": 200_000,
         "temperature": 0,
     },
     "claude-3-opus-20240229": {
         "default_max_tokens": 1000,
-        "limit_max_tokens": 200_0000,
+        "limit_max_tokens": 200_000,
         "temperature": 0,
     },
     "cohere/command-r-plus": {
-        "default_max_tokens": 1000,
-        "limit_max_tokens": 4096,
-        "temperature": 0,
-    },
-    "mistralai/mixtral-8x22b": {
         "default_max_tokens": 1000,
         "limit_max_tokens": 4096,
         "temperature": 0,
