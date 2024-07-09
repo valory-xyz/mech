4ba88c049cc765f30397af356287e63fca66f929
Peter Jung <peter@jung.ninja>
2024-07-09 14:40:32 +0200

    black

--- packages/polywrap/customs/prediction_with_research_report/prediction_with_research_report.py
+++ packages/polywrap/customs/prediction_with_research_report/prediction_with_research_report.py
@@ -182,7 +182,7 @@ def with_key_rotation(func: Callable):
             """Retry the function with a new key."""
             try:
                 result = func(*args, **kwargs)
-                return result + (api_keys, )
+                return result + (api_keys,)
             except anthropic.RateLimitError as e:
                 # try with a new key again
                 service = "anthropic"
@@ -217,8 +217,6 @@ def with_key_rotation(func: Callable):
         return mech_response
 
     return wrapper
-
-
 
 
 class CustomOpenAIEmbeddingFunction(embedding_functions.OpenAIEmbeddingFunction):
--- packages/valory/customs/native_transfer_request/native_transfer_request.py
+++ packages/valory/customs/native_transfer_request/native_transfer_request.py
@@ -35,6 +35,7 @@ client: Optional[OpenAI] = None
 
 MechResponse = Tuple[str, Optional[str], Optional[Dict[str, Any]], Any, Any]
 
+
 def with_key_rotation(func: Callable):
     @functools.wraps(func)
     def wrapper(*args, **kwargs) -> MechResponse:
@@ -47,7 +48,7 @@ def with_key_rotation(func: Callable):
             """Retry the function with a new key."""
             try:
                 result = func(*args, **kwargs)
-                return result + (api_keys, )
+                return result + (api_keys,)
             except anthropic.RateLimitError as e:
                 # try with a new key again
                 service = "anthropic"
