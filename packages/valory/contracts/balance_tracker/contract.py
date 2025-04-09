32f6a041d61226d3d8b5fdbd445cbb0f75529eb0
KahanMajmudar <kahan.m.btechi16@ahduni.edu.in>
2025-04-09 12:08:05 +0000

    adds process payment for mech

---
+++ packages/valory/contracts/balance_tracker/contract.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2023-2025 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This module contains the balance_tracker contract definition."""
+
+import logging
+from enum import Enum
+from typing import Any, Dict, List, Optional, cast
+
+from aea.common import JSONLike
+from aea.configurations.base import PublicId
+from aea.contracts.base import Contract
+from aea.crypto.base import LedgerApi
+from aea_ledger_ethereum import EthereumApi
+from web3 import Web3
+from web3.types import BlockIdentifier, TxReceipt
+
+
+PUBLIC_ID = PublicId.from_str("valory/balance_tracker:0.1.0")
+
+_logger = logging.getLogger(
+    f"aea.packages.{PUBLIC_ID.author}.contracts.{PUBLIC_ID.name}.contract"
+)
+
+PAYMENT_TYPE_NATIVE_NVM = (
+    "803dd08fe79d91027fc9024e254a0942372b92f3ccabc1bd19f4a5c2b251c316"  # nosec
+)
+PAYMENT_TYPE_TOKEN_NVM = (
+    "0d6fd99afa9c4c580fab5e341922c2a5c4b61d880da60506193d7bf88944dd14"  # nosec
+)
+
+
+class MechOperation(Enum):
+    """Operation types."""
+
+    CALL = 0
+    DELEGATE_CALL = 1
+
+
+class BalanceTrackerContract(Contract):
+    """The scaffold contract class for a smart contract."""
+
+    contract_id = PublicId.from_str("valory/balance_tracker:0.1.0")
+
+    @classmethod
+    def get_raw_transaction(
+        cls, ledger_api: LedgerApi, contract_address: str, **kwargs: Any
+    ) -> JSONLike:
+        """
+        Handler method for the 'GET_RAW_TRANSACTION' requests.
+
+        Implement this method in the sub class if you want
+        to handle the contract requests manually.
+
+        :param ledger_api: the ledger apis.
+        :param contract_address: the contract address.
+        :param kwargs: the keyword arguments.
+        :return: the tx  # noqa: DAR202
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def get_raw_message(
+        cls, ledger_api: LedgerApi, contract_address: str, **kwargs: Any
+    ) -> bytes:
+        """
+        Handler method for the 'GET_RAW_MESSAGE' requests.
+
+        Implement this method in the sub class if you want
+        to handle the contract requests manually.
+
+        :param ledger_api: the ledger apis.
+        :param contract_address: the contract address.
+        :param kwargs: the keyword arguments.
+        :return: the tx  # noqa: DAR202
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def get_state(
+        cls, ledger_api: LedgerApi, contract_address: str, **kwargs: Any
+    ) -> JSONLike:
+        """
+        Handler method for the 'GET_STATE' requests.
+
+        Implement this method in the sub class if you want
+        to handle the contract requests manually.
+
+        :param ledger_api: the ledger apis.
+        :param contract_address: the contract address.
+        :param kwargs: the keyword arguments.
+        :return: the tx  # noqa: DAR202
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def get_mech_balance(
+        cls, ledger_api: LedgerApi, contract_address: str, mech_address: str
+    ) -> JSONLike:
+        """Get mech balance"""
+        contract_instance = cls.get_instance(ledger_api, contract_address)
+        mech_balance = contract_instance.functions.mapMechBalances(mech_address).call()
+        return {"mech_balance": mech_balance}
+
+    @classmethod
+    def simulate_tx(
+        cls,
+        ledger_api: EthereumApi,
+        contract_address: str,
+        sender_address: str,
+        data: str,
+    ) -> JSONLike:
+        """Simulate the transaction."""
+        try:
+            ledger_api.api.eth.call(
+                {
+                    "from": ledger_api.api.to_checksum_address(sender_address),
+                    "to": ledger_api.api.to_checksum_address(contract_address),
+                    "data": data,
+                }
+            )
+            simulation_ok = True
+        except Exception as e:
+            _logger.info(f"Simulation failed: {str(e)}")
+            simulation_ok = False
+
+        return dict(data=simulation_ok)
+
+    @classmethod
+    def get_process_payment_tx(
+        cls,
+        ledger_api: LedgerApi,
+        contract_address: str,
+        sender_address: str,
+        mech_address: str,
+    ) -> JSONLike:
+        """Get tx data"""
+        ledger_api = cast(EthereumApi, ledger_api)
+
+        if not isinstance(ledger_api, EthereumApi):
+            raise ValueError(f"Only EthereumApi is supported, got {type(ledger_api)}")
+
+        contract_instance = cls.get_instance(ledger_api, contract_address)
+        tx_data = contract_instance.encodeABI(
+            fn_name="processPaymentByMultisig",
+            args=[mech_address],
+        )
+        simulation_ok = cls.simulate_tx(
+            ledger_api, contract_address, sender_address, tx_data
+        ).pop("data")
+        return {"data": bytes.fromhex(tx_data[2:]), "simulation_ok": simulation_ok}  # type: ignore
