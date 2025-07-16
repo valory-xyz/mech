8ca54280c69d2f334c4c3464625c9c50c2309a8b
cyberosa <arosacastillo@protonmail.com>
2025-07-16 11:19:11 +0200

    removed TODO

--- packages/napthaai/customs/prediction_request_rag/prediction_request_rag.py
+++ packages/napthaai/customs/prediction_request_rag/prediction_request_rag.py
@@ -213,8 +213,6 @@ class LLMClient:
             return response
 
         if self.llm_provider in ["openai", "openrouter"]:
-            # TODO investigate the transform parameter https://openrouter.ai/docs#transforms
-            # transform = [] # to desactivate prompt compression
             response_provider = self.client.chat.completions.create(
                 model=model,
                 messages=messages,
