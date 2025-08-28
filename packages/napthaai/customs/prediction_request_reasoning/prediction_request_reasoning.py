e59fb28cb6bfc71bf26834643363f6b52c5a5f6b
Jenslee Dsouza <dsouzajenslee@gmail.com>
2025-08-28 21:19:47 +0530

    add more logging

--- packages/napthaai/customs/prediction_request_reasoning/prediction_request_reasoning.py
+++ packages/napthaai/customs/prediction_request_reasoning/prediction_request_reasoning.py
@@ -127,6 +127,7 @@ def with_key_rotation(func: Callable) -> Callable:
                 api_keys.rotate(service)
                 return execute()
             except Exception as e:
+                print(f"Unexpected error: {e}")
                 return str(e), "", None, None, api_keys
 
         mech_response = execute()
