35956ed099c5f712be0e4ddc37aaa50916208a4b
Jenslee Dsouza <dsouzajenslee@gmail.com>
2025-11-08 02:27:28 +0530

    fix: request reasoning retry logic

--- packages/napthaai/customs/prediction_request_reasoning/prediction_request_reasoning.py
+++ packages/napthaai/customs/prediction_request_reasoning/prediction_request_reasoning.py
@@ -18,6 +18,7 @@
 # ------------------------------------------------------------------------------
 
 """This module implements a Mech tool for binary predictions."""
+import copy
 import functools
 import json
 import re
@@ -226,15 +227,16 @@ class LLMClient:
         """Generate a completion from the specified LLM provider using the given model and messages."""
         if self.llm_provider == "anthropic":
             # anthropic can't take system prompt in messages
-            for i in range(len(messages) - 1, -1, -1):
-                if messages[i]["role"] == "system":
-                    system_prompt = messages[i]["content"]
-                    del messages[i]
+            messages_copy = copy.deepcopy(messages)
+            for i in range(len(messages_copy) - 1, -1, -1):
+                if messages_copy[i]["role"] == "system":
+                    system_prompt = messages_copy[i]["content"]
+                    del messages_copy[i]
 
             response_provider = (
                 self.client.messages.create(  # pylint: disable=no-member
                     model=model,
-                    messages=messages,
+                    messages=messages_copy,
                     system=system_prompt,
                     temperature=temperature,
                     max_tokens=max_tokens,
